aefe38d4182b10eb4c76065f3a82e3dd5be81c65
tedsalvador <tedpaul@hotmail.com>
2024-06-04 20:08:45 +0200

    antes de modular

--- public/js/teams.js
+++ public/js/teams.js
@@ -35,20 +35,21 @@ function local2json(name){
 
     /* Para obtener el valor */
     cod = document.getElementById("Flags").value;
-    alert(cod);
+    // alert(cod);
      
     /* Para obtener el texto */
     combo = document.getElementById("Flags");
     selected = combo.options[combo.selectedIndex].text;
-    alert(selected);
+    // alert(selected);
 
   database.set({Equipo: + nameTeam , edad: + flagteam});
   alert("Equipo registrado..!! Gracias");
   
-  const collection = document.getElementsByTagName("li");
   // document.getElementById("demo").innerHTML = collection[1].innerHTML;
-  alert(document.collection[1].innerHTML);
-  document.collection[1].innerHTML = flagteam + ' ' + nameTeam;
+  const collection = document.getElementsByTagName("li");
+  alert(collection[6].innerHTML);
+  document.getElementsByTagName("li")[21].innerHTML = cod + ' - ' + nameTeam; /* "Hello World!";*/
+ 
   console.log(database.get());
   // return true;
   // console.log(database.get());
