66cefaa71e34e5644894643718fed2edd5e21999
tedsalvador <tedpaul@hotmail.com>
2024-06-05 01:01:41 +0200

    los datos cargados en array para la pagina classification

--- public/js/teams.js
+++ public/js/teams.js
@@ -33,6 +33,7 @@ function local2json(name){
   nameTeam = document.getElementById("TeamName").value
   flagteam = document.getElementById("Flags").value
 
+  if (validateFields() == true){
     /* Para obtener el valor */
     cod = document.getElementById("Flags").value;
     // alert(cod);
@@ -42,15 +43,15 @@ function local2json(name){
     selected = combo.options[combo.selectedIndex].text;
     // alert(selected);
 
-  database.set({Equipo: + nameTeam , edad: + flagteam});
+  database.set({Equipo: + nameTeam , Flag: + cod});
   alert("Equipo registrado..!! Gracias");
-/*   FirstItemlibre("li");
-  viewteam(6, cod, nameTeam);  */
+
   viewteam(FirstItemlibre("li"),cod, nameTeam);
   console.log(database.get());
-  // return true;
-  // console.log(database.get());
+  document.getElementById("TeamName").value="";
+  document.getElementById("Flags").selectedIndex=0;
   }
+}
 
   function FirstItemlibre (strItem){
     // const arrayTags = document.getElementsByTagName("li");
@@ -69,10 +70,40 @@ function local2json(name){
      // document.getElementById("demo").innerHTML = collection[1].innerHTML;
   const collection = document.getElementsByTagName("li");
   const btnRegister = document.getElementById("Register");
-  // alert(collection[posTeam].innerHTML);
+
   document.getElementsByTagName("li")[posTeam].innerHTML = codFlag + ' - ' + nameTeam;
+/*   localStorage.setItem("Equipo", nameTeam);
+  localStorage.setItem("Bandera", codFlag); */
   if (posTeam == 21) {
     btnRegister.disabled = true;
     btnRegister.value="Completed";
+    cargarArray();
   }
     }
+
+   function validateFields(){
+    const selectElement = document.getElementById("Flags");
+    const nameElement = document.getElementById("TeamName");
+
+if (selectElement.selectedIndex === 0 && nameElement.value == ""){
+    alert("Must be Enter a Name and select a Flag for your Team!!"); 
+    return false;
+} else if (nameElement.value == "") { 
+          alert("Enter a Name for your Team !!");
+          return false;
+}else if (selectElement.selectedIndex === 0) {
+          alert("Select a Flag for your Team !!");
+          return false;
+} else {
+  return true}
+   }
+
+   function cargarArray(){
+    let arrayTeams = new Array();
+    const arrayTags = document.getElementsByTagName("li");
+
+        for (let i = 6; i < 22; i++) {
+          const element = arrayTags[i].innerHTML;
+          arrayTeams[i-6] = element;
+        }
+   }
